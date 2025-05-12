8e5a5cdd4e8417a5441526f4b03fd86c85cbce88
Jordan Tyler <jmattyler@hotmail.com>
2025-05-11 20:32:54 -0500

    forgot to readd CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, jsonify
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/substack', methods=['GET'])
 

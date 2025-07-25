c33e9167e5dd8f4f5eb77bfe5228a1767a27034f
Jordan Tyler <jmattyler@hotmail.com>
2025-07-24 19:33:38 -0500

    removed excess comments

--- app.py
+++ app.py
@@ -5,24 +5,22 @@ import os
 
 app = Flask(__name__)
 CORS(app)
+localPort = int(5000)
 
 @app.route('/api/substack', methods=['GET'])
 
 def proxy_substack_feed():
     url = "https://voltorik.substack.com/feed"
-
     try:
-        # test network connection
-        res = requests.get(url, timeout=5)
-        res.raise_for_status() # raises error is not a 200 status
+        response = requests.get(url, timeout=5)
+        response.raise_for_status() # raises error is not a 200 status
 
-        # Return the raw XML with proper content-type
-        return Response(res.content, mimetype='application/xml')
+        return Response(response.content, mimetype='application/xml')
     except requests.exceptions.RequestException as e:
         return jsonify({"Error": "Failed to fetch RSS feed", "details":str(e)}), 502
     except Exception as e:
         return jsonify({"Error": "Internal server error", "details":str(e)}), 500
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 5000)) # deafult to 5000 for local dev
+    PORT = int(os.environ.get("PORT", localPort)) 
     app.run(host="0.0.0.0", port=PORT, debug=False)
